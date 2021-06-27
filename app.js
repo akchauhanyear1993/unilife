57944b26cc25d177076f6630bd7ad096706bf739
aws swa <aws86610@gmail.com>
2021-06-27 18:50:43 +0530

    home

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express           = require('express');
 var mongoose          = require('mongoose');
+const shortid         = require('shortid');
 var Item              = require('./models/item');
 var User              = require('./models/user');
 var nodemailer        = require('nodemailer');
@@ -23,6 +24,14 @@ const  Otp              = require('./models/otp');
 const Domain            = require('./models/domain');
 
 // event_link_user_list , friend_lists, posts, post_attachments
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "akshaychauhanofficial93@gmail.com",
+    pass: "Qazwsxedc123456",
+  },
+});
 
 
 
@@ -191,21 +200,152 @@ app.get('/country-list',(req,res)=>{
   
   
 });
+
 app.post('/signup-user',(req,res)=>{
-	    const otp = new User(req.body);
-      otp.save(function(err, user) {
-        if (err == null) return res.send({
-          status: true,
-          message: "Otp sent to mobile!",
-          data: user,
-        });
-        res.send({
-          status: false,
-          message: "Something went wrong!",
-          data: err,
-        });
-    });
+ 
+  let user_type                = req.body.user_type;
+  let university_school_id     = req.body.university_school_id;
+  let degree                  = req.body.degree;
+  let programme_name          = req.body.programme_name;
+  let current_year            = req.body.current_year;
+  let university_school_email = req.body.university_school_email;
+  let refered_by              = req.body.refered_by;
+  let name                    = req.body.name;
+  let phone                   = req.body.phone;
+  let email                   = req.body.email;
+  let parent_email            = req.body.parent_email;
+  let interest                = req.body.interest;
+  let ielts                   = req.body.interest;
+  
+  //const otp = new User(req.body);
+  
+  //************  time stamp  **************************/
+  let today = new Date();
+  let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  let dateTime = date+' '+time;
+
+  //************  time stamp  **************************/
+  
+  
+  //***** refer code generate ******/
+
+  var randStr = "";
+    for(var letter=1;letter<=5;letter++) {
+        randStr+= letter%2==0 ? String.fromCharCode(Math.random()*(91-65)+65) : Math.ceil(Math.random()*6);
+    }
+    //var randStr = "UNI"+ randStr;
+   
+    let referral_Code = randStr.toUpperCase();
+
+    let usermail = "";
+    if(user_type == "University student") {
+      usermail = university_school_email;
+    }
+    else {
+      usermail = email;
+    }
+
+    //********* send otp to email  ******************************/
+      let otpnum = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+      let otpData = {
+                'otp' : otpnum,
+                'email' : usermail,
+                'created_date' :  dateTime
+              };
+      let userData = {'user_type'       : user_type,
+                      'university_school_id' : university_school_id,
+                      'degree'          : degree,
+                      'programme_name'  : programme_name,
+                      'current_year'    : current_year,
+                      'university_school_email' : university_school_email,
+                      'name'            : name,
+                      'phone'           : phone,
+                      'email'           : usermail,
+                      'password'        : "12345",
+                      'ielts'           : ielts,
+                      'parent_email'    : parent_email,
+                      'refered_by'      : refered_by,
+                      'referral_Code'   : referral_Code,
+                      'interest'        : interest,
+                      'created_at'      : dateTime,
+                      'updated_at'      : dateTime
+       };
+       
+       const otp = new Otp(otpData);
+       const user = new User(userData);
+       otp.save().then(()=>{
+        
+           var mailOptions = {
+              from: "akshaychauhanofficial93@gmail.com",
+              to : usermail,
+              subject: "Email Verification",
+              text: "Your Email verification Otp is: " + otpnum
+              
+            };
+            transporter.sendMail(mailOptions, function (error, info) {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log("Email sent: " + info.response);
+              }
+            });
+
+            
+            user.save().then(() => {
+              //res.send({ status : true, message: 'Otp has been sent to email', data: otpData});
+            
+            }).catch((e)=>{
+              res.send(e);
+            })
+            res.send({ status : true, message: 'Otp has been sent to email', data: otpData});
+
+        }).catch((e)=>{
+         // res.send({ status : false, message: 'user has faied to signup ', data: []});
+         res.send(e);
+        })
+
+        
+
+
+
+    //***************************** send otp to email ends ******************************/
+
+  
 });
+
+
+
+
+
+
+
+
+
+// app.post('/signup-user',(req,res)=>{
+// 	    const otp = new User(req.body);
+//       const pre = "UNI";
+//       var randStr = "";
+//         for(var letter=1;letter<=5;letter++) {
+//             randStr+= letter%2==0 ? String.fromCharCode(Math.random()*(91-65)+65) : Math.ceil(Math.random()*6);
+//         }
+//         var randStr = pre+randStr;
+//        console.log(randStr.toUpperCase());
+//      otp.save(function(err, user) {
+//         if (err == null) return res.send({
+//           status: true,
+//           message: "Otp sent to mobile!",
+//           data: user,
+//         });
+//         res.send({
+//           status: false,
+//           message: "Something went wrong!",
+//           data: err,
+//         });
+//     });
+// });
+
+
 app.post('/login',(req,res)=>{
   User.find({ email: req.body.username, password : req.body.password},function(err, user) {
    
@@ -259,15 +399,6 @@ app.post('/user-device',(req,res)=>{
 
 app.post('/emailverification',(req,res)=>{
 
-  
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-    auth: {
-      user: 'abc@gmail.com',
-      pass: 'abc@12345'
-    }
-});
- 
   let usermail = req.body.email;
   let otpnum = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
   let otpData = {
@@ -277,11 +408,13 @@ var transporter = nodemailer.createTransport({
               };
     console.log(otpData);
       const otp = new Otp(otpData);
+      
       var mailOptions = {
-        from: "codertanu@gmail.com",
-        to: "amreshkumar.com@gmail.com",
+        from: "akshaychauhanofficial93@gmail.com",
+        to : usermail,
         subject: "Email Verification",
-        text: "Your Email verification Otp is: "+otpnum,
+        text: "Your Email verification Otp is: " + otpnum
+        
       };
     
       transporter.sendMail(mailOptions, function (error, info) {
@@ -317,7 +450,7 @@ app.post("/otp_verify", (req, res) => {
         var newvalues = { $set: {verify: "yes"} };
         Otp.updateOne(myquery, newvalues, function(err, res) {});
 
-
+        
         res.send({
           status: true,
           message: "OTP has verified successfully",
@@ -337,7 +470,7 @@ app.post("/otp_verify", (req, res) => {
 });
 
 
-//**** Otp Verification *******//
+//**** Domain Verification *******//
 app.post("/get_uni_id_using_domain", (req, res) => {
   let domain = req.body.domain;
   
@@ -366,7 +499,93 @@ app.post("/get_uni_id_using_domain", (req, res) => {
 });
 
 
-//**** Otp verification *******//
+//**** Domain verification ends *******//
+
+
+//******* get profile url **************/
+app.post("/get-url", (req, res) => {
+  let user_id = req.body.user_id;
+  let useremail = req.body.email;
+  
+  User.find({ _id: user_id})
+    .then((data) => {
+
+      let domaindata = data;
+      
+        var mailOptions = {
+          from: "akshaychauhanofficial93@gmail.com",
+          to : useremail,
+          subject: "Referer Code",
+          text: "Your referer Code is: " + "@gmail"+ domaindata[0].referral_Code
+          
+        };
+        
+        console.log(mailOptions.text);
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+        if(domaindata.length > 0) {
+
+          res.send({
+            status: true,
+            message: "Referer code Sent to Email."
+            
+          });
+        } else {
+          res.send({
+            status: false,
+            message: "Referer code fail to send over Email."
+            
+          });
+        }
+    })
+    .catch((e) => {
+      res.send(e);
+    });
+});
+
+
+
+//******* get profile url **************/
+
+
+//******  get_all_profile_data ************/
+
+
+app.post("/get_all_profile_data", (req , res ) => {
+  
+  let user_id = req.body.user_id;
+
+  User.find({ _id: user_id})
+    .then((data) => {
+      let userdata = data; // for user data 
+        University.find({_id : userdata.university_school_id} ).then((unidata) => {
+          console.log(unidata);
+        })
+        .catch((e) => {
+          res.send(e);
+        });
+    })
+    .catch((e) => {
+      res.send(e);
+    });
+
+
+
+
+});
+
+app.post("homepage_data", (req, res) => {
+
+});
+
+
+
+
 
 
 
--- models/user.js
+++ models/user.js
@@ -3,30 +3,30 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
   
-  usertype: {
+  user_type: {
     type: String,
-    required: false,
+    required: true
 
   },
-  university_name: {
-    type: String,
-    required: false,
+  // university_name: {
+  //   type: String,
+  //   required: false,
 
-  },
+  // },
   is_online : {
     type: String,
-    required: false,
+    required: true,
     default : "online"
 
   },
   degree: {
     type: String,
-    required: false,
+    required: true
 
   },
   programme_name: {
     type: String,
-    required: false,
+    required: true
 
   },
   current_year: {
@@ -36,40 +36,97 @@ var userSchema = new Schema({
   },
   otp_verify : {
     type: String,
-    required: true,
-    unique: "yes"
+    required: false,
+    default: "no"
 
   },
+  designation : {
+    type: String,
+    required: false,
+    default: ""
+
+  },
+  organisation : {
+    type: String,
+    required: false,
+    default: ""
+
+  },
+  personal_mission : {
+    type: String,
+    required: false,
+    default: ""
+
+  },
+  personal_description : {
+    type: String,
+    required: false,
+    default: ""
+  },
+  profile_banner_image : {
+    type: String,
+    required: false,
+    default: ""
+
+  },
+  date_of_birth : {
+    type : String,
+    required : false,
+    default : ""
+  },
+  gender : {
+    type: String,
+    required: false,
+    default: ""
+
+  },
+  
   university_school_email: {
       type: String,
-      required: true,
-      unique: true,
+      required: false,
+      default: ""
+  },
+  email_domain : {
+    type: String,
+    required: false,
+    default: ""
+
+  },
+
+  email :{
+    type : String,
+    required : true
   },
   referral_Code: {
     type: String,
-    required: true,
-    default: "offline"
+    required: false,
+    default: ""
   },
+  refered_by: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  interest : {
+    type: String,
+    required: false,
+    default: ""
+  },
+
   name: {
     type: String,
     required: false
     },
   phone: {
         type: String,
-        required: true,
-        default: "no",
-        unique: true
-    },
-  email: {
-        type: String,
         required: false,
-        default : "",
-        unique: true,
-        
+        default: "no",
+       
     },
+  
   parent_email: {
         type: String,
-        required: true,
+        required: false,
         default: "public"
     },
     profile_status : {
@@ -77,27 +134,94 @@ var userSchema = new Schema({
         required: false,
         default: ""
     },
-    university_school_id : {
-      type: Number,
-        required: false,
-        default: "0"
+    status : {
+      type: String,
+      required: false,
+      default: "active"
 
     },
+  university_school_id : {
+      type: String,
+      required: false,
+      default: ""
+
+  },
+
+  
+  username : {
+    type: String,
+    required : false,
+    default : ""
+
+  },
+  ielts : {
+    type : String,
+    required : false
+
+  },
+  complete_profile : {
+    type : String ,
+    required : false,
+    default : ""
+  },
+  profile_image : {
+    type : String ,
+    required : false,
+    default : ""
+  },
 
   country: {
         type: String,
-        required: true
+        required: false,
+        default : ""
        
     },
-  created_on: {
-        type: Number,
-        required: true
-    },
-
     password: {
         type: String,
-        required: true
+        required: false,
+        default : ""
+    },
+    decoded_password : {
+      type: String,
+        required: false,
+        default : ""
+    },
+    reset_password : {
+      type: String,
+      required: false,
+      default : ""
+
+    },
+    remember_token : {
+      type: String,
+      required: false,
+      default : ""
+
+    },
+
+    created_at : {
+      type: String,
+      required: false,
+      default: ""
+    }, 
+    updated_at : {
+      type: String,
+      required: false,
+      default: ""
+    },
+    source : {
+      type: String,
+      required: false,
+      default: ""
+  
+    },
+    version : {
+      type: Number,
+      required: false,
+      default: 2.0
+  
     }
+
 
 }, { collection: 'user' });
 
